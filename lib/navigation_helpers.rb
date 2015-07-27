2aa0fa5e543f3323903e2f52322c0d642cdbee16
Donny Reynolds <donnywith@gmail.com>
2015-07-27 00:18:24 -0700

    adds breadcrumb_title option for shorter titles in breadcrumbs

--- lib/navigation_helpers.rb
+++ lib/navigation_helpers.rb
@@ -1,7 +1,7 @@
 module NavigationHelpers
   def breadcrumbs_list(resource, list=[])
     if resource
-      list.prepend( url: resource.url, name: resource.data.title )
+      list.prepend( url: resource.url, name: resource.data.breadcrumb_title || resource.data.title )
       breadcrumbs_list(resource.parent, list)
     else
       list[0...-1]
